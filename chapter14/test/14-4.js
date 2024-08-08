5809de7b8f1ae7a83757fe2f28e2325c50ec0214
Paekma09 <paekma09@gmail.com>
2024-08-08 14:57:17 +0900

    chapter14-1_done

---
+++ chapter14/test/14-4.js
@@ -0,0 +1,21 @@
+/* 타임아웃 기본 설정 때문에 실패하는 테스트 */
+const assert = require('assert');
+
+function asyncFunc(cb) {
+  setTimeout(function () {
+    // 3초 후 콜백 함수에 foo를 전달
+    cb('foo');
+  }, 3000);
+}
+
+describe('비동기 코드 테스트', function () {
+  describe('#setTimeout', function () {
+    it('2초를 지나 done 함수가 호출되어 실패하는 테스트', function (done) {
+      asyncFunc(function (result) {
+        assert.ok(result === 'foo');
+        // done 함수 호출이 기본 제한 시간인 2초를 지나 실패한다.
+        done();
+      });
+    });
+  });
+});
---
+++ chapter14/test/14-7.js
@@ -0,0 +1,20 @@
+/* async / await 테스트 */
+const assert = require('assert');
+
+function returnPromise() {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve('foo');
+    }, 1000);
+  });
+}
+
+describe('비동기 코드 테스트', function () {
+  describe('async / await 테스트', function () {
+    // async 함수를 인자로 사용
+    it('1초 후 값을 전달 받는 함수', async function () {
+      const result = await returnPromise();
+      assert.ok(result === 'foo');
+    });
+  });
+});
